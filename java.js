0aec243b3f71c047088157a217fb0bb3c582adf3
Ahmadziad121 <ahmadziad758@gmail.com>
2024-02-15 13:51:02 +0300

    Create java.js

---
+++ java.js
@@ -0,0 +1,29 @@
+// function sq(a){
+//     for(let i=0;i<10;i++)
+//     {
+//         if(i*i)>a
+//         return i
+//     }    
+    
+// }
+// //sq(10)
+// /*function fact(a){
+//     var f=1
+//     for(let i=1;i<=a;i++){
+//     f=f*i}
+//       return (f)
+// }
+// console.log(fact(5))*/
+// function cum(a){
+//     var f=0
+//     for(let i=1;i<=a;i++){
+//     f=f+i}
+//       return (f)
+// }
+// console.log(cum(5))
+
+function click7(){
+    alert("this is your email")
+}
+
+
